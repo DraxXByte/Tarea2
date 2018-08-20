9a41b7209337346324040b58a1bcce3f4a47a7c9
RamirezJuarezArturo5im9 <42345770+RamirezJuarezArturo5im9@users.noreply.github.com>
2018-08-20 18:13:14 -0500

    Add files via upload

---
+++ Tarea2-Ramirez_Juarez_Arturo-5IM9/src/tarea2/ramirez_juarez_arturo/pkg5im9/Tarea2Ramirez_Juarez_Arturo5IM9.java
@@ -0,0 +1,10 @@
+package tarea2.ramirez_juarez_arturo.pkg5im9;
+
+public class Tarea2Ramirez_Juarez_Arturo5IM9 {
+    public static void main(String[] args) {
+     System.out.println("Hola Mundo");
+     System.out.println("Ramirez Juarez Arturo Yamil");
+     System.out.println("5IM9 Tarea2");
+    }
+    
+}
